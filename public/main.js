67ba97d38bd46b17c1733334b9d33317ce597835
Reese Parker <parker.rhys@outlook.com>
2022-03-10 16:24:30 -0500

    Initial commit

---
+++ public/main.js
@@ -0,0 +1,15 @@
+const nameElement = document.querySelector(".name");
+const jobTitleElement = document.querySelector(".job-title");
+const comingSoonElement = document.querySelector(".coming-soon");
+
+setTimeout(() => {
+  nameElement.style.color = "white";
+}, 700);
+
+setTimeout(() => {
+  jobTitleElement.style.color = "white";
+}, 1500);
+
+setTimeout(() => {
+  comingSoonElement.style.color = "white";
+}, 2500);
